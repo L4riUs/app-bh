3466360b3003a252f500d50b69128c4b2402f11a
garnicaluis391 <garnicaluis391@gmail.com>
2025-10-22 13:07:33 +0000

    update. Integracion de tunnel para uso de app en linea

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,8 @@
 import { YStack, H1, Text, Button, ScrollView } from "tamagui";
-// import { useTheme } from "../src/state/themeContext";
 import LoginScreen from "../src/features/login/components/login";
 
 export default function HomeScreen() {
-  // const { theme, toggleTheme } = useTheme();
   return (
-    // <YStack
-    //   flex={1}
-    //   alignItems="center"
-    //   justifyContent="center"
-    //   backgroundColor="$background"
-    //   padding="$4"
-    //   gap="$4"
-    // >
-    //   <H1>¡Hola Tamagui!</H1>
-    //   <Text fontSize="$5">Tema actual: {theme}</Text>
-    //   <Button
-    //     size="$4"
-    //     theme="blue"
-    //     animation="bouncy"
-    //     pressStyle={{ scale: 0.95 }}
-    //     onPress={toggleTheme}
-    //   >
-    //     Cambiar Tema
-    //   </Button>
-    // </YStack>
     <LoginScreen />
   );
 }
