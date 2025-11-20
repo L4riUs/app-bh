54b5b9a2c47aa288641f4b920d7f402c4c245cf1
garnicaluis391 <garnicaluis391@gmail.com>
2025-11-20 02:56:57 +0000

    update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,22 @@
-import { XStack, Image } from 'tamagui'
-import { Bell } from "@tamagui/lucide-icons"
+import { XStack, Image, Button } from 'tamagui'
+import { Bell, Moon, Sun } from "@tamagui/lucide-icons"
+import { useTheme } from '@state/themeContext'
+
+
+function BtnDarkMode() {
+    const { toggleTheme, theme } = useTheme()
+    if (theme == "dark") {
+        return <Moon size={25} onPress={toggleTheme} />
+    } else {
+        return <Sun size={25} onPress={toggleTheme} />
+    }
+}
 export default function Header() {
     return (
         <XStack alignItems='center' gap={10}>
             <Bell size={25} />
-            <Image source={require("@assets/1.jpg")} width={35} height={35} borderRadius={50}/>
+            <BtnDarkMode />
+            <Image source={require("@assets/1.jpg")} width={35} height={35} borderRadius={50} />
         </XStack>
     )
 }
